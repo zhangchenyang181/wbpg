2e7dd0c166c623bf51d4c2f1644d0903e47989b6
zhangchenyang181 <zhangchenyang181@mails.ucas.ac.cn>
2021-01-10 17:24:52 +0800

    test

--- train.py
+++ train.py
@@ -1,1 +1,12 @@
 #微博个性化文本生成
+def parse_args():
+    	'''
+	Parse Seq2seq with attention arguments.
+	'''
+	parser = argparse.ArgumentParser(description="Run PCGN training.")
+
+	parser.add_argument('--config', nargs='?',
+						default='./configs/pcgn_config.yaml',
+						help='Configuration file for model specifications')
+
+	return parser.parse_args()
